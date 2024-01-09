20034ccec5245c38b3c36cf74cc27fb5be1fdb50
CodeConnects <codeconnects@gmail.com>
2024-01-09 15:22:53 -0400

    complete variable demo

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -8,3 +8,17 @@
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
+
+const courseName = 'typeScript';
+
+const toothFairy = 2.0;
+
+let teeth = 32;
+{
+  let teeth = 29;
+  console.log('teeth in function: '+teeth);
+}
+console.log('teeth outside function: '+teeth);
+
+// let can be initialized without a value and then assigned later
+// const must be initialized with a value and cannot be reassigned
