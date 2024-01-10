ef1046701023e05d050f383b73eeae7056e4a14a
CodeConnects <codeconnects@gmail.com>
2024-01-10 11:08:56 -0400

    complete compound boolean demo

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,3 +1,17 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+const coding = true;
+const reading = !coding;
+
+const age = 18;
+const isTeen = age >= 13 && age < 20;
+assert.equal(isTeen, true);
+
+const pckWeight = 10;
+const pckLength = 20;
+const pckWidth = 30;
+const feeExempt = false;
+
+const additionalFee = !feeExempt && (pckWeight > 10 || pckLength > 50 || pckWidth > 30);
+assert.equal(additionalFee, true);
